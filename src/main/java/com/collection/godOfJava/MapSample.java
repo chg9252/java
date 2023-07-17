e478ef999cf528cbd67859f976d6e2d70695ce95
Naerok <chg9252@gmail.com>
2023-07-17 23:53:36 +0900

    [추가] collection_Map_set

--- src/main/java/com/collection/godOfJava/MapSample.java
+++ src/main/java/com/collection/godOfJava/MapSample.java
@@ -1,6 +1,8 @@
 package com.collection.godOfJava;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 public class MapSample {
     public static void main(String[] args) {
@@ -12,5 +14,34 @@ public class MapSample {
     private void checkHashMap() {
         HashMap<String,String> map=new HashMap<String,String>();
         map.put("A", "a");
+        System.out.println(map.get("A"));
+        System.out.println(map.get("B"));
     }
+
+    public void checkkeyset() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("A", "a");
+        map.put("c", "c");
+        map.put("D", "d");
+        Set<String> keySet = map.keySet();
+        for (String tempKey : keySet) {
+
+            System.out.println(tempKey + "=" + map.get(tempKey));
+        }
+    }
+
+    public void checkValues() {
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("A", "a");
+        map.put("C", "c");
+        map.put("D", "d");
+
+        Collection<String> values=map.values();
+        for(String tempValue:values) {
+            System.out.println(tempValue);
+        }
+    }
+
 }
+
